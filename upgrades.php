fa16ba70c48a1af7d6771b7b31bb231b3f75628f
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2015-02-13 19:41:09 -0800

    Emails and all

--- upgrades.php
+++ upgrades.php
@@ -19,11 +19,11 @@ define( 'CORE_UPGRADE_SKIP_NEW_BUNDLED', true );
 add_filter( 'auto_update_plugin', '__return_true' );
 
 // Force auto theme updates
-add_filter( 'auto_update_theme', '__return_true' );
+//add_filter( 'auto_update_theme', '__return_true' );
 
-// Suspend emails
-//add_filter( 'auto_core_update_send_email', '__return_false', 1 );
-//add_filter( 'automatic_updates_send_debug_email', '__return_false', 1 );
+// Suspend or force emails (false == no email ; true == yes email)
+add_filter( 'auto_core_update_send_email', '__return_true', 1 );
+add_filter( 'automatic_updates_send_debug_email', '__return_true', 1 );
 
 // SSL fixes
 //add_filter('https_ssl_verify', '__return_false');
