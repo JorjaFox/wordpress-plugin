151d8043f177f3333981e6357ece6ab83144d121
Mika Epstein (Ipstenu) <ipstenu@halfelf.org>
2018-04-03 15:51:57 -0700

    Fixing some colors because of Google

--- utility-pro/functions.php
+++ utility-pro/functions.php
@@ -18,7 +18,7 @@ class FLF_Utility_Pro {
 	 */
 	public function __construct() {
 
-		self::$version = '1.3.8';
+		self::$version = '1.3.9';
 
 		// Actions
 		add_action( 'wp_head', array( $this, 'header' ) );
