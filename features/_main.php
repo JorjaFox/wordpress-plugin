834c9b9469f12580804b79deda0096c3223955a1
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2021-09-21 13:13:50 -0700

    Typo in filename

--- features/_main.php
+++ features/_main.php
@@ -2,7 +2,7 @@
 
 require_once 'jetpack.php';
 require_once 'shortcodes.php';
-require_once 'spamers.php';
+require_once 'spammers.php';
 require_once 'tvmaze.php';
 require_once 'upgrades.php';
 require_once 'videos.php';
