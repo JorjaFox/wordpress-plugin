01218def6f5244cfd9f69c62ad0e5e58f2a3dac0
Mika Epstein <ipstenu@halfelf.org>
2022-06-21 14:15:42 -0700

    FormattingNinja Forms

--- features/ninja-forms.php
+++ features/ninja-forms.php
@@ -5,10 +5,18 @@
  * https://dzone.com/articles/register-hidden-fields-ninja
  */
 
- function flf_register_ninja_form_fields() {
+function flf_register_ninja_form_fields() {
 	$args_ip = array(
 		'name'             => 'User IP',
-		'display_function' => 'collect_user_ip_display',
+		'display_function' => 'flf_collect_user_ip_display',
+		'sidebar'          => 'template_fields',
+		'display_label'    => false,
+		'display_wrap'     => false,
+	);
+
+	$args_page = array(
+		'name'             => 'Page',
+		'display_function' => 'flf_collect_user_page',
 		'sidebar'          => 'template_fields',
 		'display_label'    => false,
 		'display_wrap'     => false,
@@ -16,6 +24,7 @@
 
 	if ( function_exists( 'ninja_forms_register_field' ) ) {
 		ninja_forms_register_field( 'user_ip', $args_ip );
+		ninja_forms_register_field( 'user_page', $args_page );
 	}
 }
 
@@ -38,6 +47,26 @@ function flf_collect_user_ip_display( $field_id, $data ) {
 				<label for="ninja_forms_field_<?php echo esc_attr( $field_id ); ?>">User IP</label>
 				<input type="text" name="ninja_forms_field_<?php echo esc_attr( $field_id ); ?>" value="<?php echo esc_attr( $data['default_value'] ); ?>">
 				<p><a href="http://whois.domaintools.com/<?php esc_attr( $data['default_value'] ); ?>" target="_blank">Find out more about this person</a></p>
+			</div>
+		<?php
+	}
+}
+
+function flf_collect_user_page( $field_id, $data ) {
+	global $post;
+
+	if ( !empty( $post ) ) {
+		?>
+		<!-- Yes, we track your IPs if you fill in the form. This should not shock anyone. -->
+		<input type="hidden" name="ninja_forms_field_<?php echo esc_attr( $field_id ); ?>" value="<?php echo (int) get_permalink( $post->ID ); ?>">
+		<?php
+	}
+
+	if ( is_admin() ) {
+		?>
+			<div class="field-wrap text-wrap label-above">
+				<label for="ninja_forms_field_<?php echo esc_attr( $field_id ); ?>">Page Form Submitted</label>
+				<input type="text" name="ninja_forms_field_<?php echo esc_attr( $field_id ); ?>" value="<?php echo esc_attr( $data['default_value'] ); ?>">
 			</div>
 		<?php
 	}
