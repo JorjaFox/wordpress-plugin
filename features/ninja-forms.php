4f713dc34569e68077ec180807912778202e23e7
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2022-10-19 11:39:31 -0700

    This works but I don't like it.

--- features/ninja-forms.php
+++ features/ninja-forms.php
@@ -1,44 +1,81 @@
 <?php
-
 /**
- * Support IPs in Ninja Forms.
- * https://dzone.com/articles/register-hidden-fields-ninja
+ * Ninja Forms
  */
 
- function flf_register_ninja_form_fields() {
-	$args_ip = array(
-		'name'             => 'User IP',
-		'display_function' => 'collect_user_ip_display',
-		'sidebar'          => 'template_fields',
-		'display_label'    => false,
-		'display_wrap'     => false,
-	);
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
 
-	if ( function_exists( 'ninja_forms_register_field' ) ) {
-		ninja_forms_register_field( 'user_ip', $args_ip );
+
+class FLF_NinjaForms {
+	/**
+	 * On load.
+	 */
+	public function __construct() {
+		add_filter( 'ninja_forms_submit_data', array( $this, 'comment_blocklist' ) );
 	}
+
+	/**
+	 * List of disallowed emails
+	 *
+	 * We omit anything that isn't an email address or has an @ in the string.
+	 *
+	 * @return array the list
+	 */
+	public static function list() {
+		$disallowed_emails = array();
+		$disallowed_array  = explode( "\n", get_option( 'disallowed_keys' ) );
+
+		// Make a list of spammer emails and domains.
+		foreach ( $disallowed_array as $spammer ) {
+			if ( is_email( $spammer ) || ( strpos( $spammer, '@' ) !== false ) ) {
+				// Anything with an @-symbol is probably an email, so let's trust it.
+				$disallowed_emails[] = trim( $spammer );
+			}
+		}
+
+		return $disallowed_emails;
+	}
+
+	/**
+	 * Ninja Forms: Server side email protection using WordPress comment blocklist
+	 * https://developer.ninjaforms.com/codex/custom-server-side-validation
+	 *
+	 * @param array $form_data Form data array.
+	 * @return array $form_data email checked form data array.
+	 */
+	public function comment_blocklist( $form_data ) {
+		$disallowed = self::list();
+
+		foreach ( $form_data['fields'] as $field ) {
+			// If this is email, we will do some playing.
+			if ( 'email' === $field['key'] ) {
+				$email_address = sanitize_email( strtolower( $field['value'] ) );
+
+				// Break apart email into parts
+				$emailparts = explode( '@', $email_address );
+				$username   = $emailparts[0];       // i.e. foobar
+				$domain     = '@' . $emailparts[1]; // i.e. @example.com
+
+				// Remove all periods (i.e. foo.bar > foobar )
+				$clean_username = str_replace( '.', '', $username );
+
+				// Remove everything AFTER a + sign (i.e. foobar+spamavoid > foobar )
+				$clean_username = ( false !== strpos( $clean_username, '+' ) ) ? strstr( $clean_username, '+', true ) : $clean_username;
+
+				// rebuild email now that it's clean.
+				$email = $clean_username . '@' . $emailparts[1];
+
+				// If the email OR the domain is an exact match in the array, then it's a spammer
+				if ( in_array( $email, $disallowed, true ) || in_array( $domain, $disallowed, true ) ) {
+					//$form_data['_honeypot'] = 'I suck';
+					$form_data['errors']['fields'][ $field['id'] ] = 'Error: Invalid data.';
+				}
+			}
+		}
+		return $form_data;
+	}
+
 }
 
-add_action( 'init', 'flf_register_ninja_form_fields' );
-
-function flf_collect_user_ip_display( $field_id, $data ) {
-	global $post;
-
-	$id = $_SERVER['REMOTE_ADDR'];
-
-	if ( ! empty( $post ) ) {
-		?>
-			<input type="hidden" name="ninja_forms_field_<?php echo esc_attr( $field_id ); ?>" value="<?php echo esc_attr( $id ); ?>">
-		<?php
-	}
-
-	if ( is_admin() ) {
-		?>
-			<div class="field-wrap text-wrap label-above">
-				<label for="ninja_forms_field_<?php echo esc_attr( $field_id ); ?>">User IP</label>
-				<input type="text" name="ninja_forms_field_<?php echo esc_attr( $field_id ); ?>" value="<?php echo esc_attr( $data['default_value'] ); ?>">
-				<p><a href="http://whois.domaintools.com/<?php esc_attr( $data['default_value'] ); ?>" target="_blank">Find out more about this person</a></p>
-			</div>
-		<?php
-	}
-}
+new FLF_NinjaForms();
