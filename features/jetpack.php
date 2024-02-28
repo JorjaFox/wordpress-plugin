b658655efaa9b0810b67c51c713c7b31c6b8fe20
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2024-02-28 12:24:04 -0800

    kill AI

--- features/jetpack.php
+++ features/jetpack.php
@@ -15,3 +15,16 @@ JS;
         wp_add_inline_script( 'jetpack-blocks-editor', $disable_external_media );
     }
 );
+
+// Kill AI in Jetpack.
+add_filter( 'jetpack_ai_chat_enabled', 'helf_jetpack_disable_ai' );
+add_filter( 'jetpack_ai_enabled', 'helf_jetpack_disable_ai' );
+
+/**
+ * Disable Jetpack AI. Evil!
+ *
+ * @return bool
+ */
+function helf_jetpack_disable_ai() {
+	return false;
+}
